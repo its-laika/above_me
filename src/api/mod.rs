7aa71967250c1bad57c7d2c27343e548f8d2c109
Curly <36010519+neon-JS@users.noreply.github.com>
2024-03-16 10:32:25 +0100

    chore: Rename mod

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,8 +1,8 @@
-pub use router::init_api_server;
+pub use server::init_api_server;
 
 pub const MAX_AGE_DIFF: u64 = 60 * 5; /* 5 minutes */
 pub const POSITION_RADIUS: f32 = 0.05; /* approx. 5 - 6 km */
 
 mod handler;
-mod router;
+mod server;
 mod state;
