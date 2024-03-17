e3e7af045e7534e137e6d954b7d2229856c04290
Curly <36010519+neon-JS@users.noreply.github.com>
2024-03-17 22:29:27 +0100

    refactor: Removed api::handler
    It's now so simple that it can be included in api::server. Therefore move it.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,5 @@
 pub use server::init_api_server;
 pub use state::AppState;
 
-mod handler;
 mod server;
 mod state;
--- src/api/server.rs
+++ src/api/server.rs
@@ -1,5 +1,11 @@
-use super::{handler::default_handler, state::AppState};
-use axum::{routing::get, Router};
+use super::state::AppState;
+use crate::aprs::Status;
+use axum::{
+    extract::{Path, State},
+    routing::get,
+    Json, Router,
+};
+use serde::Serialize;
 use std::io::Error;
 use tokio::{net::ToSocketAddrs, sync::oneshot};
 
@@ -9,7 +15,7 @@ pub async fn init_api_server<'a, A: ToSocketAddrs>(
     shutdown_rx: oneshot::Receiver<()>,
 ) -> Result<(), Error> {
     let app = Router::new()
-        .route("/r/:latitude/:longitude/:range", get(default_handler))
+        .route("/r/:latitude/:longitude/:range", get(handler))
         .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind(address).await?;
@@ -21,4 +27,24 @@ pub async fn init_api_server<'a, A: ToSocketAddrs>(
         .await?;
 
     Ok(())
+}
+
+async fn handler(
+    Path((latitude, longitude, range)): Path<(f32, f32, f32)>,
+    State(app_state): State<AppState>,
+) -> Json<ResponseDto> {
+    Json(ResponseDto {
+        latitude,
+        longitude,
+        range,
+        states: app_state.get_filtered_states(latitude, longitude, range),
+    })
+}
+
+#[derive(Serialize)]
+struct ResponseDto {
+    latitude: f32,
+    longitude: f32,
+    range: f32,
+    states: Vec<Status>,
 }
